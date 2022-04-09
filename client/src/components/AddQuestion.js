6f4e5053cfef74dd781263d0264cf265e5c7416f
sarahbhura <67743793+sarahbhura@users.noreply.github.com>
2022-04-09 14:11:17 +0500

    hafsah's changes to addQuestion.js

--- client/src/components/AddQuestion.js
+++ client/src/components/AddQuestion.js
@@ -1,67 +1,75 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AddQuestion = () => {
-       let navigate = useNavigate()
+  let navigate = useNavigate();
 
-    const [question, setQuestion] = useState({ 
-        description:"",
-        alternatives:[{
-          "text":"",
-          "isCorrect":""
-      }]
-
-    });  
-        
+  const [question, setQuestion] = useState({
+    description: "",
+    alternatives: [
+      {
+        text: "",
+        isCorrect: "",
+      },
+    ],
+  });
 
   //const {description, alternative: {text, isCorrect}} = question;
   //console.log(text);
 
-  const onInputChange = e => {
-    
-    setQuestion({...question,[e.target.name]: e.target.value})
-    
-
-  }
+  const onInputChange = (e) => {
+    setQuestion({ ...question, [e.target.name]: e.target.value });
+  };
 
   const addAlternative = (e) => {
-    let temp = {...question};
-    temp.alternatives.push(
-      {
-        text: "",
-        isCorrect: ""
-      }
-    );
+    let temp = { ...question };
+    temp.alternatives.push({
+      text: "",
+      isCorrect: "",
+    });
+    setQuestion(temp);
+  };
+  /*
+    const deleteQuestion = (e, i) => {
+      let temp = {...state}
+      temp.branches.splice(i, 1)
+      setState(temp)
+    }
+  */
+  const deleteAlternative = (e, j) => {
+    let temp = { ...question };
+    temp.alternatives.splice(j, 1);
     setQuestion(temp);
   };
 
   const handleAlternativeChange = (e, i) => {
-    let temp = {...question}
-    temp.alternatives[i][e.target.name] = e.target.value
-    setQuestion(temp)
-  } 
-  
-const onSubmit = async e => {
-    
-    e.preventDefault()
-    await axios.post('http://localhost:4000/question/createQuestion',question,
-          {headers: {
-          'Authorization': 'Bearer ' + localStorage.getItem('auth'),
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-          
-      }})
-    navigate('/QuizDashboard');
-  
-  }
+    let temp = { ...question };
+    temp.alternatives[i][e.target.name] = e.target.value;
+    setQuestion(temp);
+  };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(
+      "http://localhost:4000/question/createQuestion",
+      question,
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("auth"),
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    navigate("/QuizDashboard");
+  };
 
-    return(
-        <div className="container">
-        <div className="w-75 mx-auto shadow p-5">
-          <h2 className="text-center mb-4">Add A Question</h2>
-          <form onSubmit={e =>onSubmit(e) }>
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add A Question</h2>
+        <form onSubmit={(e) => onSubmit(e)}>
           <div className="form-group">
             <input
               type="text"
@@ -69,35 +77,55 @@ const onSubmit = async e => {
               placeholder="Enter Question Description"
               name="description"
               value={question.description}
-              onChange ={e => onInputChange(e)} 
-              
+              onChange={(e) => onInputChange(e)}
             />
           </div>
-          <br/>
-          {
-        question.alternatives.map((alternative, i) => (
-          <div style={{padding: '25px'}}>
-            <span style={{fontSize: '18px'}}>Option {i+1}: </span>
-            <input variant='outlined' name='text' placeholder='answer text'
-               onChange={e => handleAlternativeChange(e, i)} value={question.alternatives[i].text}
-            />
-            
-            <input variant='outlined' name='isCorrect' placeholder='true false?'
-               onChange={e => handleAlternativeChange(e, i)} value={question.alternatives[i].isCorrect}
-            />
-           <div>
-              <br/>
+          <br />
+          {question.alternatives.map((alternative, i) => (
+            <div style={{ padding: "25px" }}>
+              <span style={{ fontSize: "18px" }}>Option {i + 1}: </span>
+              <input
+                variant="outlined"
+                name="text"
+                placeholder="answer text"
+                onChange={(e) => handleAlternativeChange(e, i)}
+                value={question.alternatives[i].text}
+              />
+
+              <input
+                variant="outlined"
+                name="isCorrect"
+                placeholder="true false?"
+                onChange={(e) => handleAlternativeChange(e, i)}
+                value={question.alternatives[i].isCorrect}
+              />
+              <div>
+                <br />
+              </div>
+              <button
+                variant="contained"
+                color="primary"
+                onClick={(e) => addAlternative(e, i)}
+              >
+                Add Option
+              </button>
+              <button
+                variant="contained"
+                color="secondary"
+                style={{ marginLeft: "10px" }}
+                onClick={(e) => deleteAlternative(e, i)}
+              >
+                Delete option
+              </button>
             </div>
-            <button variant='contained' color='primary' onClick={e => addAlternative(e, i)}>Add Option</button>
-          </div>
-        ))
-      }
-          <br/>
+          ))}
+          <br />
+
           <button className="btn btn-primary btn-block">Add Question</button>
         </form>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default AddQuestion;
--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -1,50 +1,48 @@
-import axios from 'axios';
-import React, {useState} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./AddTeacher.css";
 
 const AddStudent = () => {
   let navigate = useNavigate();
-  const [Student,setStudent] = useState({
-    firstName:"",
-    lastName:"",
-    role:"student",
-    user_id:"",
-    password:""
-  })
+  const [Student, setStudent] = useState({
+    firstName: "",
+    lastName: "",
+    role: "student",
+    user_id: "",
+    password: "",
+  });
 
-const {firstName,lastName,role,user_id,password} = Student; 
-const onInputChange = e => {
-  setStudent({...Student,[e.target.name]: e.target.value})
+  const { firstName, lastName, role, user_id, password } = Student;
+  const onInputChange = (e) => {
+    setStudent({ ...Student, [e.target.name]: e.target.value });
+  };
 
-}
-
-const onSubmit = async e => {
-  e.preventDefault()
-  await axios.post('http://localhost:4000/admin/registerStudent',Student,
-        {headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('auth'),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        
-    }})
-  navigate('/AdminDashboard/studentPage');
-
-}
-    return(
-        <div className="container">
-      <div className="w-75 mx-auto shadow p-5">
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:4000/admin/registerStudent", Student, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("auth"),
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    navigate("/AdminDashboard/studentPage");
+  };
+  return (
+    <div className="container-main  ">
+      <div className="container-form shadow">
         <h2 className="text-center mb-4">Add a Student</h2>
-        <form onSubmit={e =>onSubmit(e) }>
-
+        <form onSubmit={(e) => onSubmit(e)}>
           <div className="form-floating mb-3 ">
             <input
               type="text"
               className="form-control form-control-lg mb-2"
               id="floatingInput"
               name="firstName"
-              placeholder='Enter First Name'
+              placeholder="Enter First Name"
               value={firstName}
-              onChange ={e => onInputChange(e)} 
+              onChange={(e) => onInputChange(e)}
             />
             <label for="floatingInput">First Name</label>
           </div>
@@ -55,13 +53,12 @@ const onSubmit = async e => {
               className="form-control form-control-lg mb-2"
               id="floatingInput"
               name="lastName"
-              placeholder='Enter First Name'
+              placeholder="Enter Last Name"
               value={lastName}
-              onChange ={e => onInputChange(e)} 
+              onChange={(e) => onInputChange(e)}
             />
             <label for="floatingInput">Last Name</label>
           </div>
-
 
           <div className="form-floating mb-3 ">
             <input
@@ -69,11 +66,11 @@ const onSubmit = async e => {
               className="form-control form-control-lg mb-2"
               id="floatingInput"
               name="Role"
-              placeholder='Enter Role'
-              value='Student'
-              onChange ={e => onInputChange(e)} 
+              placeholder="Enter Role"
+              value="Student"
+              onChange={(e) => onInputChange(e)}
             />
-            <label for="floatingInput">Role</label>
+            <label for="floatingInput"> Role</label>
           </div>
 
           <div className="form-floating mb-3 ">
@@ -82,13 +79,12 @@ const onSubmit = async e => {
               className="form-control form-control-lg mb-2"
               id="floatingInput"
               name="user_id"
-              placeholder='Enter User ID'
+              placeholder="Enter User ID"
               value={user_id}
-              onChange ={e => onInputChange(e)} 
+              onChange={(e) => onInputChange(e)}
             />
             <label for="floatingInput">User ID</label>
           </div>
-
 
           <div className="form-floating mb-3 ">
             <input
@@ -98,19 +94,21 @@ const onSubmit = async e => {
               placeholder="Enter Initial password"
               name="password"
               value={password}
-              onChange ={e => onInputChange(e)} 
+              onChange={(e) => onInputChange(e)}
             />
             <label for="floatingInput">Password</label>
           </div>
 
-          <button className="btn btn-primary btn-block me-2 mb-2">Add Student</button>
+          <button className="btn btn-primary btn-block me-2 mb-2">
+            Add Student
+          </button>
           <Link className="btn btn-primary me-2 mb-2" to="/adminDashboard">
-                Back to Dashboard
-            </Link>
+            Back to Dashboard
+          </Link>
         </form>
       </div>
     </div>
-    )
+  );
 };
 
 export default AddStudent;
