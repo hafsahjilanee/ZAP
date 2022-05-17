2e78c18c0c3a0a4ba8e6fbc919ceb283c080fb1a
hafsahjilanee <hafsah2006@hotmail.com>
2022-05-17 10:18:42 +0500

    commit -m "fixed routes"

--- client/src/App.js
+++ client/src/App.js
@@ -139,7 +139,7 @@ function App() {
             />
             <Route
               exact
-              path="/TeacherDashboard/TeacherCoursePage:id"
+              path="/TeacherDashboard/TeacherCoursePage"
               element={<TeacherCoursePage />}
             />
             <Route
